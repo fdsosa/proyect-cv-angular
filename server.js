023bb6c7ed681b2fe8d6b088b5e315fff74dec7e
Federico <fdsosa.35@gmail.com>
2019-10-08 18:14:41 -0300

    Move server.js file

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/my-dream-app'));
+app.use(express.static(__dirname + '/dist/my-dream-app'));
+
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/my-dream-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/my-dream-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
