111935ca4637ad3f73e1111c9794b0c72d8e29ba
Federico <fdsosa.35@gmail.com>
2019-10-23 12:53:21 -0300

    Fixed a error with the dir name

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/my-app'));
+app.use(express.static(__dirname + '/dist/my-dream-app'));
 app.use(cors());
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/my-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/my-dream-app/index.html'));
 });
 
 app.get('/index',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/my-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/my-dream-app/index.html'));
 });
 
 app.get('/repo',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/my-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/my-dream-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
